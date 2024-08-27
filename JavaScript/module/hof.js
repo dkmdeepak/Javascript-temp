a487dc9caa043f78f253dff4b519e33deab4bb02
Deepak <dkmdeepak8137@gmail.com>
2024-08-27 20:10:09 +0530

    'first'

--- JavaScript/module/hof.js
+++ JavaScript/module/hof.js
@@ -1,22 +1,44 @@
-const sum = withMarkers((a,b)=>{
-    console.log(a + b);
-});
-const product = withMarkers((a,b)=>{
-    console.log(a * b);
-});
-const difference = withMarkers((a,b)=>{
-    console.log(a - b);
-});
+// const sum = withMarkers((a,b)=>{
+//     console.log(a + b);
+// });
+// const product = withMarkers((a,b)=>{
+//     console.log(a * b);
+// });
+// const difference = withMarkers((a,b)=>{
+//     console.log(a - b);
+// });
 
 
-const wrappedSum= withMarkers(sum);
-function withMarkers(func){
-    return function(a, b){
-        console.log('=======');
-        sum(a, b);
-        console.log('=======');
-    }
-}
+// const wrappedSum= withMarkers(sum);
+// function withMarkers(func){
+//     return function(a, b){
+//         console.log('=======');
+//         sum(a, b);
+//         console.log('=======');
+//     }
+// }
 // function sum(a,b){
 //     console.log(a + b);
 // }
+
+// const postName=[{
+//     name:'david',
+//     age:20,
+//     git:'global'
+// },{
+//     name:'davids',
+//     age:20,
+//     git:'globals'
+// },]
+
+// function dash(){
+//     setTimeout(()=>{
+//         let output='';
+//         postName.forEach((token,value)=>{
+//             output=`${token.name} <br/>`
+//         },1000)
+//         document.body.innerHTML=output
+//     })
+// }
+// dash();
+
