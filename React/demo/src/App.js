15a471dc8fb2f626c7337bdca29af381d8dd1ddb
Deepak <dkmdeepak8137@gmail.com>
2024-08-30 20:06:52 +0530

    'first'

--- React/demo/src/App.js
+++ React/demo/src/App.js
@@ -1,11 +1,24 @@
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name='Peter'
+  const valid=true
+  const valids=false
+  const placeholder="Enter here"
   return (
+    <>
     <div className="App">
-      <h1>hw</h1>
+      <h1 style={{backgroundColor:'green',color:'white'}}>dash</h1>
     </div>
+    <div style={{color:'black'}}>
+      <h1 style={{color:'blueviolet'}}>Hello-{name}</h1>
+      <label>Type Here- 
+        <input placeholder={placeholder} className='main' type='text' />
+      </label>
+      <h3>{valid?'True':'False'}</h3>
+      <h3>{valids?'True':'False'}</h3>
+    </div>
+  </>
   );
 }
 
