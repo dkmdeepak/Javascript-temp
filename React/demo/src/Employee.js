42ecc57c878faa0010f39fe253de75264e97e30c
Deepak <dkmdeepak8137@gmail.com>
2024-09-01 00:37:32 +0530

    'first'

--- React/demo/src/Employee.js
+++ React/demo/src/Employee.js
@@ -1,12 +1,32 @@
 import './App.css'
 
 function Employee(){
+    let inputData="";
+
+    const dash=()=>{
+        alert('Ding Dong')
+    }
+    const btnClick=(arg)=>{
+        alert(arg)
+    }
+    const inpVal=(val)=>{
+        inputData=val
+        console.log(inputData);
+    }
+    const btnClk=()=>{
+        alert(inputData)
+    }
     return(
         <>
         <div>
             <h1 className='meal'>Happy Meal</h1>
             <h1 className='meal'>Welcome</h1>
-            <h1 className='meal'>Super Star</h1>
+            <button onClick={dash}>Click</button><br />
+            <button onClick={()=>{
+                btnClick('Binding With argument')
+            }}>Click Me!!</button>
+            <input type="text" onChange={(e)=>{inpVal(e.target.value)}}/>
+            <button onClick={btnClk}>Event</button>
         </div>
         </>
     )
