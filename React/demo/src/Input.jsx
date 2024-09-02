d5bcfb9235dd2ca128acf112ec0e28bfa07ee33c
Deepak <dkmdeepak8137@gmail.com>
2024-09-02 22:13:58 +0530

    'first'

--- React/demo/src/Input.jsx
+++ React/demo/src/Input.jsx
@@ -1,16 +1,27 @@
 function Input(){ 
     let insert=''
+
     const handle=(val)=>{
         insert=val;
     }
+
     const change=()=>{
         console.log(insert);
+        // alert(insert)
+        try{
+            alert(eval(insert))
+        }
+        catch{
+            alert('Invalid Expression')
+        }
     }
+ 
     return(
         <>
         <input type="text" onChange={(e)=>{
             handle(e.target.value)
         }} /><br />
+        <h1>{insert}</h1>
         <button onClick={change}>Click Me</button>
         </>
     )
