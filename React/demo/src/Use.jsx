a5daad7c9fb943852cf88f773195b50e2240b8a6
Deepak <dkmdeepak8137@gmail.com>
2024-09-05 17:43:54 +0530

    'first'

---
+++ React/demo/src/Use.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function Use(url) {
+    const [data,setData]=useState([])
+    fetch(url).then(res=>res.json).then(res=>{
+        setData(res)
+    })
+  return data
+}
+
+export default Use
