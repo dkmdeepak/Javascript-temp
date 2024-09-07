b8ee5fd6d66b621183267a4f9b2446817464a148
Deepak <dkmdeepak8137@gmail.com>
2024-09-07 23:56:24 +0530

    'first'

---
+++ React/demo/src/User.jsx
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+class User extends Component{
+    render(){
+        return(
+            <>
+            <h1>Class Component</h1>
+            </>
+        )
+    }
+}
+export default User;
